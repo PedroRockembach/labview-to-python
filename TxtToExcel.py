7fe6d1e5f21451e8b03ad7c3974c588ebab8076b
PedroRockembach <rockpedro11@gmail.com>
2025-10-02 10:01:56 -0300

    Iterando cada passo

--- TxtToExcel.py
+++ TxtToExcel.py
@@ -3,17 +3,17 @@ import openpyxl
 
 texto_exemplo = 'Teste15_C1_Pressao_TCH25630013-C1-FOLGA-0,85mm-Cavidade-5-TesteDia29.txt'
 
-def _txtToExel(exemplo):
-    lista_passagem = []
+def _txtToExel(txt_pressao):
+    lines = []
     
-    with open(exemplo,"r") as ex:
-        for i in ex:
-            lista_passagem.append(i)
+    with open(txt_pressao,"r") as ex:
+        for line in ex:
+            if line.strip():
+                columns = line.strip().split('\t')
+                lines.append(columns)
+        return lines
             
-            if lista_passagem[i] == '\n':
-                print('\n')
-            else:    
-                print(lista_passagem)
+        
         
 if __name__ == "__main__":
     _txtToExel(texto_exemplo)
